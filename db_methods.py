d66f49f308dbfc2ea12a73686442804d4ead154b
galencolin <galencolin@gmail.com>
2019-11-09 16:58:13 -0500

    asdfa

---
+++ db_methods.py
@@ -0,0 +1,143 @@
+import sqlite3
+import random
+import time
+
+conn = sqlite3.connect('teenhacks.db', check_same_thread = False)
+
+c = conn.cursor()
+
+def setup():
+    #product table
+    #id - food id
+    #name - food name
+    #tag - food tag
+    #desc - food description
+    #cid - company id
+    #link - food link
+    c.execute('''create table if not exists products (id text, price text, name text, tag text, desc text, cid text, link text, zip text, county text)''')
+    #company table
+    #cid - company id
+    #name - company name
+    #uname - username
+    #psw - password
+    #phone/email - contact info
+    c.execute('''create table if not exists companies (cid text, name text, uname text, psw text, phone text, email text, zip text, county text, state text)''')
+    #student table
+    #sid - student id
+    #name - student name
+    #uname - username
+    #psw - password
+    #uni - university
+    #gid - group id
+    c.execute('''create table if not exists students (sid text, name text, uname text, psw text, uni text, zip text, county text, state text, gid text)''')
+    #master group table
+    #gid - group id
+    #gname - group name
+    c.execute('''create table if not exists master_groups (gid, gname)''')
+    conn.commit()
+    
+
+def genID(l=[]):
+    while True:
+        s = ''
+        for i in range(50):
+            t = random.randint(0,2)
+            if (t == 0):
+                s += chr(ord('0') + random.randint(0, 9))
+            elif (t == 1):
+                s += chr(ord('a') + random.randint(0, 25))
+            else:
+                s += chr(ord('A') + random.randint(0, 25))
+        if (s not in l):
+            l.append(s)
+            return s
+
+def getlink(name):
+    return "link not found"
+
+def company_login(uname, pswd):
+    v = c.execute('''SELECT * FROM companies WHERE uname='{}' AND psw='{}' '''.format(uname, pswd))
+    return v.fetchall()
+
+def student_login(uname, pswd):
+    v = c.execute('''SELECT * FROM students WHERE uname='{}' AND psw='{}' '''.format(uname, pswd))
+    return v.fetchall()
+
+#zc is zipcode
+def add_student(name, uname, psw, uni, zc, county, state, gid = ""):
+    c.execute('''INSERT INTO students values('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')'''.format(genID(), name, uname, psw, uni, zc, county, state, gid))
+    conn.commit()
+
+def create_group(name):
+    gid = genID()
+    c.execute('''INSERT INTO master_groups values('{}', '{}')'''.format(gid, name))
+    c.execute('''CREATE TABLE '{}' (id text, timestamp text, content text)'''.format(gid))
+    conn.commit()
+
+def create_announcement(gid, content):
+    timestamp = time.strftime("%a, %d %b %Y %I:%M", time.localtime())
+    c.execute('''INSERT INTO {} values('{}', '{}', '{}')'''.format(gid, genID(), timestamp, content))
+    conn.commit()
+
+def get_announcement(gid):
+    announcements = []
+    info = c.execute('''SELECT * FROM {}'''.format(gid))
+    for row in info:
+        announcements.append(list(row))
+
+    return announcements
+    
+
+#zc is zipcode
+def add_company(name, uname, psw, phone, email, zc, county, state):
+    c.execute('''INSERT INTO companies values('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')'''.format(genID(), name, uname, psw, phone, email, zc, county, state))
+    conn.commit()
+
+def add_product(price, name, tag, desc, cid):
+    company_info = c.execute('''SELECT * FROM companies WHERE cid="{}"'''.format(cid));
+    for row in company_info:
+        #print(row)
+        info = row
+    zc, county = info[6], info[7]
+    c.execute('''INSERT INTO products values('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')'''.format(genID(), price, name, tag, desc, cid, getlink(name), zc, county));
+    conn.commit()
+
+def get_tags(keyword):
+    tags = []
+    products = c.execute('''SELECT * FROM products''')
+    for row in products:
+        #add nlp later
+        if keyword in row[3] and row[3] not in tags:
+            tags.append(row[3])
+
+    return tags
+
+def get_products(tag, sid):
+    products = []
+    student_info = c.execute('''SELECT * FROM students WHERE sid = "{}"'''.format(sid))
+    for row in student_info:
+        info = row
+    zc = info[5]
+    items = c.execute('''SELECT * FROM products WHERE tag = '{}' AND zip = '{}' '''.format(tag, zc))
+    for row in items:
+        products.append(list(row))
+
+    return products
+
+def clear_db():
+    tables = c.execute('SELECT * FROM sqlite_master WHERE type="table"')
+    table_names = []
+    for row in tables:
+        table_names.append(row[1])
+    for t in table_names:
+        c.execute("DROP TABLE '{}'".format(t))
+        print("dropped {}".format(t))
+    conn.commit()
+
+def close():
+    conn.close()
+
+if __name__ == '__main__':
+    setup()
+    print(verify_student_login("a", "a"))
+    close()
---
+++ server.py
@@ -0,0 +1,211 @@
+import flask, sys, logging, random
+from flask import render_template, request, jsonify, Flask, make_response, redirect
+import db_methods as db
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+ID_LEN = 100
+
+### Head - done
+
+def genID(id_len = ID_LEN, l = []):
+    while True:
+        s = ''
+        for i in range(id_len):
+            t = random.randint(0,2)
+            if (t == 0):
+                s += chr(ord('0') + random.randint(0, 9))
+            elif (t == 1):
+                s += chr(ord('a') + random.randint(0, 25))
+            else:
+                s += chr(ord('A') + random.randint(0, 25))
+        if (s not in l):
+            l.append(s)
+            return s
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='./public',
+            template_folder='./public')
+			
+### Add product page - awaiting aaron
+
+# Send HTML, process form await aaron
+@app.route('/add_product', methods=['GET', 'POST'])
+def add_product():
+	if (request.method == 'GET'):
+		res = make_response(render_template('add_product.html'))
+		res.set_cookie('id', 'a4')
+		res.set_cookie('type', 'company')
+		return res
+	else:
+		type = request.cookies.get('type')
+		if (not type or type != 'company'):
+			return redirect('/invalid_company')
+			
+		name = request.form['name']
+		price = request.form['price']
+		desc = request.form['desc']
+		tag = request.form['tag']
+		
+		id = request.cookies.get('id')
+		
+		print("add_product", price, name, tag, desc, id)
+		
+		# later add no back button - https://stackoverflow.com/questions/20652784/flask-back-button-returns-to-session-even-after-logout
+		return redirect('/add_product')
+
+# Get list of tags await aaron
+@app.route('/product_tags', methods=['GET'])
+def product_tags():
+	tags = get_tags()
+	return jsonify({'tags': tags})
+		
+# Delete after await aaron
+def get_tags():
+	return ['tag1', 'tab2']
+
+### Register/login progress
+
+@app.route('/register_student', methods=['GET', 'POST'])
+def register_student():
+	if (request.method == 'GET'):
+		return render_template('register_student.html')
+	else:
+		uname = request.form['uname']
+		pword = request.form['pword']
+		name = request.form['name']
+		university = request.form['university']
+		county = request.form['county']
+		zip = request.form['zip']
+		state = request.form['state']
+		
+		db.add_student(name, uname, pword, university, zip, county, state)
+		print('register student', name, uname, pword, university, zip, county, state)
+		
+		rows = db.student_login(uname, pword)
+		res = make_response('ok')
+		res.set_cookie('id', rows[0][0])
+		res.set_cookie('type', 'student')
+		return res
+		
+@app.route('/register_company', methods=['GET', 'POST'])
+def register_company():
+	if (request.method == 'GET'):
+		return render_template('register_company.html')
+	else:
+		uname = request.form['uname']
+		pword = request.form['pword']
+		name = request.form['name']
+		phone = request.form['phone']
+		email = request.form['email']
+		county = request.form['county']
+		zip = request.form['zip']
+		state = request.form['state']
+		
+		db.add_company(name, uname, pword, phone, email, zip, county, state)
+		print('register company', name, uname, pword, phone, email, zip, county, state)
+		
+		rows = db.company_login(uname, pword)
+		res = make_response('ok')
+		res.set_cookie('id', rows[0][0])
+		res.set_cookie('type', 'company')
+		return res
+
+@app.route('/login_student', methods=['GET', 'POST'])
+def login_student():
+	if (request.method == 'GET'):
+		return render_template('login_student.html')
+	else:
+		uname = request.form['uname']
+		pword = request.form['pword']
+		
+		rows = db.student_login(uname, pword)
+		
+		if (len(rows) == 0):
+			res = make_response('ok')
+			res.set_cookie('id', rows[0][0])
+			res.set_cookie('type', 'student')
+			print('login student ok', uname, pword)
+		else:
+			res = make_response('Account already exists!')
+			print('login student taken', uname, pword)
+		return res
+
+### Student hub await sujay
+
+# Send HTML await login
+@app.route('/student_hub')
+def student_hub():
+	res = make_response(render_template('student_hub.html'))
+	res.set_cookie('id', 'b7')
+	res.set_cookie('type', 'student')
+	
+	# type = request.cookies.get('type')
+	# if (not type or type != 'student'):
+		# return redirect('/invalid_student')
+	return res
+
+# Get announcements await aaron
+@app.route('/list_announcements', methods=['GET'])
+def list_announcements():
+	type = request.cookies.get('type')
+	if (not type or type != 'student'):
+		return redirect('/invalid_student')
+	
+	id = request.cookies.get('id')
+	
+	list = get_announcements(id)
+	
+	return jsonify({'announcements': list})
+	
+def get_announcements(id):
+	return [['a', 'b', 'c'], ['d', 'e', 'f']]
+	
+@app.route('/list_recipes', methods=['GET'])
+def list_recipes():
+	query = request.args.get('query')
+	
+	print('get recipes', query)
+	
+	recipes = get_recipes(query)
+	
+	return jsonify({'recipes': recipes})
+	
+def get_recipes(query):
+	return ['2', '4']
+	
+
+### Get products page
+
+# @app.route('/product_list', methods=['GET'])
+# def product_list():
+	# query = request.args.get('something')
+	
+# def get_products():
+	# return [
+		# ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'],
+		# ['dwa', 'dwa', 'fqwasd', 'fwa', 'wad3qy', 't2qfwas', '2gtqwf', 'y2gew', '54jher']
+	# ]
+
+### Invalid pages
+
+@app.route('/invalid_student')
+def invalid_student():
+	return 'Invalid request, you are not a student'
+	
+@app.route('/invalid_company')
+def invalid_company():
+	return 'Invalid request, you are not a company'
+	
+### General database methods - get rid of when merging
+
+def login(id):
+	return [['a', 'b', 'c', 'd', 'e']]
+
+### Run
+		
+if __name__ == "__main__":
+	app.run(host = 'localhost', port = 4004)
+
